5adb334c7ff606681a2080f9885c441f5552aec0
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-04-18 21:04:26 -0600

    working on home page

---
+++ src/containers/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import AppBar from 'material-ui/AppBar';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppBar
+            title="Title"
+        />
+      </div>
+    );
+  }
+}
+
+export default Home;
