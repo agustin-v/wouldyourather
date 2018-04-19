c120db7621135c2b3f6cac4cc4ebb3e645495c02
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-04-18 21:02:58 -0600

    render routes or login page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withRouter } from 'react-router-dom'
 
+import Routes from './Routes'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  return(
+    <Routes/>
+  )
 }
 
-export default App;
+export default withRouter(App)
