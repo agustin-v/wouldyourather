653a25ecfe2831963cecd545b79138f50d5632a0
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-04-19 21:13:34 -0600

    yarn eject

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -9,11 +9,11 @@ import styles from './loginForm.css'
 const LoginForm = () => {
     return(
         <div styleName='login-form'>
-            <DropDownMenu  >
+            <div><h2>Choose a user:</h2></div>
+            <DropDownMenu  style={{width: '50%' }}>
                 <MenuItem value={1} primaryText="Never" />
                 <MenuItem value={2} primaryText="Every Night" />
             </DropDownMenu>
-
         </div>
     )
 }
