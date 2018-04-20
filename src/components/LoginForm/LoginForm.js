e5cdec9d7e874b610377f1a80361fb2395f465b2
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-04-19 21:16:51 -0600

    LoginForm css

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -9,11 +9,13 @@ import styles from './loginForm.css'
 const LoginForm = () => {
     return(
         <div styleName='login-form'>
-            <div><h2>Choose a user:</h2></div>
-            <DropDownMenu  style={{width: '50%' }}>
-                <MenuItem value={1} primaryText="Never" />
-                <MenuItem value={2} primaryText="Every Night" />
-            </DropDownMenu>
+            <div style={{textAlign: 'center'}}><h2>Choose a user:</h2></div>
+            <div style={{textAlign: 'center'}}>
+                <DropDownMenu  style={{width: '50%' }}>
+                    <MenuItem value={1} primaryText="Never" />
+                    <MenuItem value={2} primaryText="Every Night" />
+                </DropDownMenu>
+            </div>
         </div>
     )
 }
