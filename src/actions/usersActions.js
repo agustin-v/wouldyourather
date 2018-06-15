25e0a106af8427eb277c16b0fcaa755636bd609c
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-06-15 11:51:10 -0600

    adding loadUsers()

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,8 +1,18 @@
+import { _getUsers} from '../data/_DATA'
 export const RECEIVE_USERS = 'RECEIVE_USERS'
 
 export function receiveUsers (users) {
     return {
         type: RECEIVE_USERS, 
         users,
+    }
+}
+
+export function loadUsers() {
+    return (dispatch) => {
+        return _getUsers()
+        .then((users) =>{
+            dispatch(receiveUsers(users))
+        })
     }
 }
