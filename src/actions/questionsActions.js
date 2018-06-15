0d7939318691411a43cccd3c0b5c397f12094ceb
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-06-15 14:45:04 -0600

    adding loadquestions action creator

--- src/actions/questionsActions.js
+++ src/actions/questionsActions.js
@@ -1,8 +1,19 @@
+import { _getQuestions} from '../data/_DATA'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 
-export function receiveTweets (questions) {
+export function receiveQuestions (questions) {
     return{
         type: RECEIVE_QUESTIONS,
         questions,
+    }
+}
+
+export function loadQuestions() {
+    return (dispatch) => {
+        return _getQuestions()
+        .then((questions) =>{
+            dispatch(receiveQuestions(questions))
+        })
     }
 }
