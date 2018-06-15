bc1c7d2d8dee907616569465c85ad6a4f42709ce
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-06-15 11:48:27 -0600

    Connecting container to state and generating the array of users that will be displayed in the login form

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { login } from '../actions/userActions'
 
 import LoginForm from '../components/LoginForm/LoginForm'
 
-const Login = () => {
+const Login = ({users, dispatch }) => {
+
+    const authUser = (user) => {
+        dispatch(login(user))
+    }
+
     return(
         <div>
-            <LoginForm />
+            <LoginForm 
+                users = { users }
+                authUser = { authUser }
+            />
         </div>
     )
 }
 
-export default Login
+const mapStateToProps = ({users}) => {
+    return {
+      users: Object.keys(users).map( user => { return { id: users[user].id, name:  users[user].name, avatarURL: users[user].avatarURL}})
+    }
+  }
+
+export default connect(mapStateToProps)(Login)
