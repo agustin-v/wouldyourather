6a824307709dd36c7a8e4779e7d6fef6ef574ea9
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-06-15 10:32:38 -0600

    switching to thunks

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,16 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+import thunk from 'redux-thunk'
 import history from '../store/configureHistory'
 import logger from 'redux-logger'
 import { routerMiddleware } from 'react-router-redux'
 
 import reducers from '../reducers';
-import sagas from '../sagas';
 
 
 export default function configureStore() {
-    const sagaMiddleware = createSagaMiddleware()
-    let middleware = applyMiddleware(sagaMiddleware, routerMiddleware(history), logger);
+    
+    let middleware = applyMiddleware(thunk, routerMiddleware(history), logger);
 
     if (process.env.NODE_ENV !== 'production') {
         const devToolsExtension = window.devToolsExtension;
@@ -20,7 +19,6 @@ export default function configureStore() {
     }
 
     const store = createStore( reducers, middleware )
-    sagaMiddleware.run(sagas)
     return store;
 }
 
