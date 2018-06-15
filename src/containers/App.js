43a52793c609d7bee1b60754d3e36b2cd16287fd
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-06-15 14:27:02 -0600

    using fragments to contain the elements in the app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { loadUsers} from '../actions/usersActions'
+import { loadUsers } from '../actions/usersActions'
+import { loadQuestions } from '../actions/questionsActions'
+import { logout } from '../actions/userActions'
+
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import FlatButton from 'material-ui/FlatButton';
 
 import Routes from './Routes'
 import Login from './Login'
 
 class App extends React.Component  {
 
+    logout = () => {
+        this.props.dispatch(logout())
+    }
+
     componentDidMount() {
         this.props.dispatch(loadUsers())
+        this.props.dispatch(loadQuestions())
     }
-    
+
     render () {
-        const { authenticated } = this.props
+        const { authenticated, user } = this.props
         return(
-            authenticated ? <Routes/> : <Login/>
+            authenticated 
+                ? <React.Fragment>
+                    <AppBar
+                        title={`${user.name}, would you rather...`}
+                        iconElementLeft={<IconButton><ActionHome /></IconButton>}
+                        iconElementRight={<FlatButton label="Logout" onClick={(e) => this.logout() }/>} 
+                    /> 
+                    <Routes/>
+                </React.Fragment>
+                : <Login/>
         
         )
     }
@@ -23,7 +44,8 @@ class App extends React.Component  {
 
 const mapStateToProps = (state) => {
   return {
-    authenticated: state.authentication.authenticated
+    authenticated: state.authentication.authenticated,
+    user: state.authentication.user
   }
 }
 
