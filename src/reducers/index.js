fe3e1defe25c5b44589a45cf9cfbd10eeb79b1f6
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-06-15 10:47:19 -0600

    questions reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 
 import { authentication } from './userReducer';
 import { users } from './usersReducer'
+import { questions } from './questionsReducer'
 
 const rootReducer = combineReducers({
     authentication,
     users,
+    questions,
 });
 
 export default rootReducer;
