23ee6a7286ba9b991721e145f00e42de0bea9456
Agustin Villalobos <agustinvillalobos@protonamil.com>
2018-06-16 12:41:51 -0600

    Leaderboard completed

--- src/containers/Leaderboard.js
+++ src/containers/Leaderboard.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
-const Leaderboard = () => {
+import Paper from 'material-ui/Paper';
+import {List, ListItem} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+
+const Leaderboard = ({ users }) => {
     return (
-        <div></div>
+        <div className='container'>
+            <Paper style={ {textAlign: 'jsutify', margin: '20px', padding: '20px'} } zDepth={1} rounded={false}>
+                <List>
+                    {
+                        users.map(user => {
+                            return <ListItem
+                                key={user.id}
+                                leftAvatar={<Avatar src={user.avatarURL} />}
+                                primaryText={user.name}
+                                secondaryText={`Asked: ${user.questions.length}, Answered: ${Object.keys(user.answers).length}`}
+                            />
+                        })
+                    }
+                </List>
+            </Paper>
+        </div>
     )
 }
 
-export default Leaderboard
+const mapStateToProps = ({users}) => {
+   
+    return {
+        users: Object.keys(users).map( user => { return users[user]}).sort((a, b) => { return (b.questions.length + Object.keys(b.answers).length) - (a.questions.length + Object.keys(a.answers).length)})
+    }
+}
+
+export default connect(mapStateToProps)(Leaderboard);
